192ceda4e5c92b2a4b43d99fbdbf6cc3831bf7a6
elyonousimohamed <m.el.yonousi@ibhorizons.com>
2022-11-12 01:06:02 +0100

    update github rep

--- server/api/github_repo.ts
+++ server/api/github_repo.ts
@@ -1,6 +1,6 @@
 export default defineEventHandler(async () => {
 	const { githubApi, accessTokens } = useRuntimeConfig()
-	const repos = ['el-yonousi', 'youtube', 'aiche_bois', 'dikri', 'doing', 'nuxt3_demo_project', 'movie-nuxt']
+	const repos = ['el-yonousi', 'youtube', 'aiche_bois', 'dikri', 'doing']
 
 	return await Promise.all(repos.map(name =>
 		fetch(`${githubApi}${name}`, {
